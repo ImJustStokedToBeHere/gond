43508fd588ab520346be5139c40d4a974f3cebe5
Joe Vidunas <jvidunas@outlook.com>
2024-08-08 01:46:06 -0400

    Add project files.

---
+++ gond-cons/gond-cons.cpp
@@ -0,0 +1,12 @@
+﻿// gond-cons.cpp : Defines the entry point for the application.
+//
+
+#include "gond-cons.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
